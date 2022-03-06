d22d51a0f11dcfbfa8b052daba05a5123bfe9451
Aissam Barchil <abarchil@e1r2p5.1337.ma>
2022-03-06 20:14:42 +0100

    test

--- main.c
+++ main.c
@@ -28,6 +28,44 @@ int worldMap[mapWidth][mapHeight]=
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
+int	search_for_player(t_parsing *parsing, int i, int j, int count)
+{
+	while (parsing->map[i])
+	{
+		j = 0;
+		while (parsing->map[i][j])
+		{
+			if (parsing->map[i][j] != '1' && parsing->map[i][j]
+				!= '0' && parsing->map[i][j] != 'N' && parsing->map[i][j]
+				!= 'S' && parsing->map[i][j] != 'E' && parsing->map[i][j]
+				!= 'W')
+				return (free_parsing(parsing),
+					printf("Error:Map Error: 0x10\n"), 1);
+			else if (parsing->map[i][j] == 'N' || parsing->map[i][j] == 'S'
+			|| parsing->map[i][j] == 'E' || parsing->map[i][j] == 'W')
+				count++;
+			j++;
+		}
+		i++;
+	}
+	if (count != 1)
+		return (free_parsing(parsing), printf("Error:Map Error: 0x10\n"), 1);
+	return (0);
+}
+
+void	ft_debug(t_parsing *parsing)
+{
+	int	i;
+
+	i = 0;
+	printf("%s\n%s\n%s\n%s\n", parsing->no, parsing->we, parsing->ea, parsing->so);
+	printf("F %d %d %d | C %d %d %d\n", parsing->floor_red,
+		parsing->floor_green, parsing->floor_blue, parsing->ceiling_red,
+		parsing->ceiling_green, parsing->ceiling_blue);
+	while (parsing->map[i])
+		printf("-%s-\n", parsing->map[i++]);
+}
+
 static int key_handle(int key, t_cub *d)
 {
     double  old_dir_x;
@@ -100,25 +138,41 @@ int	ft_exit(void)
 int main(int ac, char **av)
 {
     t_cub   data;
-
+	int 	index;
+	int 	jndex;
     data = (t_cub){0};
-    t_parsing parsing;
+   t_parsing	parsing;
+	index = -1;
+	jndex = -1;
 
-	if (parsing_directions_colors(&parsing, av, ac) || skip_newlines(&parsing))
+	if (parsing_directions_colors(&parsing, av, ac)
+		|| skip_newlines(&parsing))
 		return (1);
-	parse_map(&parsing);
-	parse_map_assign(&parsing);
-	if (check_map(&parsing))
-		return 1;
+	parse_map(&parsing, 0);
+	parse_map_assign(&parsing, 0);
+	if (check_map(&parsing, 0, 0))
+		return (1);
 	replace_spc_wall(&parsing);
-	if (search_for_player(&parsing))
-		return 1;
+	if (search_for_player(&parsing, 0, 0, 0))
+		return (1);
 	close(parsing.fd);
-	free_parsing(&parsing);
-    // BETA
-    for (int i = 0; i < mapHeight; i++)
-        for (int j = 0; j < mapWidth; j++)
-            data.worldMap[i][j] = worldMap[i][j];
+	data.worldMap = (int **)malloc(sizeof(int *) * parsing.count_lines);
+	while (++index < parsing.count_lines)
+		data.worldMap[index] = (int *)malloc(sizeof(int ) * parsing.biggest_lines);
+	index = -1;
+   while (++index < parsing.count_lines)
+   {
+	   while (++jndex < parsing.biggest_lines)
+	   {
+		   if (parsing.map[index][jndex] == '0')
+				data.worldMap[index][jndex] = 0;
+	   		else if (data.worldMap[index][jndex])
+				data.worldMap[index][jndex] ='1';
+	   }
+   }
+    // for (int i = 0; i < parsing.count_lines; i++)
+    //     for (int j = 0; j < parsing.biggest_lines; j++)
+    //         data.worldMap[i][j] = worldMap[i][j];
     data.pos[X] = 22;
     data.pos[Y] = 12;
     data.dir[X] = -1;
@@ -131,5 +185,6 @@ int main(int ac, char **av)
     mlx_hook(data.win_ptr, 2, 0, key_handle, &data);
     mlx_hook(data.win_ptr, 17, 2, &ft_exit, &data);
     mlx_loop(data.mlx_ptr);
+	free_parsing(&parsing);
     return (0);
 }
